3043a02226edfa327ef811c989cde5a5b3fa469b
陈 <50291874+1375632510@users.noreply.github.com>
2020-11-09 15:34:52 +0800

    Add files via upload

---
+++ eureka/src/main/java/com/cy/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:  {一句话描述类是干什么的}<br/>
+ *
+ * @author chenyu
+ * @version 1.0
+ * @date: 2020/11/9 14:28
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
