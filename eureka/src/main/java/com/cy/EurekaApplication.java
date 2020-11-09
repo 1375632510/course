3d1f3e1dc4b5ea135f3ae738be5d6c7a64796cfb
1375632510 <chenyu@analysys.com.cn>
2020-11-09 15:59:46 +0800

    system子项目建立，eureka错误问题处理

--- eureka/src/main/java/com/cy/EurekaApplication.java
+++ eureka/src/main/java/com/cy/EurekaApplication.java
@@ -1,7 +1,11 @@
 package com.cy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 /**
  * Description:  {一句话描述类是干什么的}<br/>
@@ -12,8 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since JDK 1.8
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+//        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication application = new SpringApplication(EurekaApplication.class);
+        Environment env = application.run(args).getEnvironment();
+        logger.info("启动成功！");
+        logger.info("Eurekad地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 }
